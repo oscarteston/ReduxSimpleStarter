53f5360d9732a530f4605720d2a408218af9c4fd
Oscar Testón Rodríguez <oscar.teston@accedo.tv>
2017-05-24 17:06:25 +0200

    finish react initi example

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,6 +8,7 @@ class searchBar extends Component {
     
     constructor(props) {
         super(props);
+        this.props = props;
 
         this.state = {
             term: ''
@@ -17,18 +18,17 @@ class searchBar extends Component {
     }
 
     onInputChange(event) {
-        let value = event.target.value;
+        let term = event.target.value;
 
-        this.setState(prevState => ({
-            term: value
-        }));
+        this.setState(prevState => ({term}));
+
+        this.props.onSearchTermChange(this.state.term);
     }
 
     render() {
         return (
-            <div>
+            <div className="search-bar">
                 <input value={this.state.term} onChange={this.onInputChange}/>
-                Value of input: {this.state.term}
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyDDNVSYCxyCWsOctzIcLCNYd-DRRAW7vrg';
-
-YTSearch(
-    {
-        key: API_KEY,
-        term: 'surfboards'
-    },
-    function(data) {
-        console.log(data);
-    }
-)
-
 
 class App extends Component {
 
@@ -22,22 +13,39 @@ class App extends Component {
         super(props);
         
         this.state = {
-            videos: []
+            videos: [],
+            selectedVideo: null
         };
 
-        YTSearch({key: API_KEY,term: 'surfboards'},(videos) => {
-                this.setState((prevState)=>({
-                    videos
-                }));
-            }
-        )
+        this.onVideoSelect = this.onVideoSelect.bind(this);
+
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY,term: term},(videos) => {
+            this.setState((prevState)=>({
+                videos: videos,
+                selectedVideo: videos[0]
+            }));
+        });
+    }
+
+    onVideoSelect(selectedVideo) {
+        this.setState({selectedVideo})
     }
 
     render() {
+        const videoSearch = _.debounce((term)=>{this.videoSearch(term)}, 300);
+
         return (
             <div>
-                <SearchBar />
-                <VideoList videos={this.state.videos}/>
+                <SearchBar onSearchTermChange={videoSearch}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList 
+                    onVideoSelect = {this.onVideoSelect}
+                    videos={this.state.videos}
+                />
             </div>
         )
     }
